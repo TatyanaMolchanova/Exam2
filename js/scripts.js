648fa9ccc7a7fcec3a63107c098d029938a046f6
Molchanova Tatyana <mtuspeh@gmail.com>
2019-08-27 06:50:50 +0300

    first commit project not finished 3 sections

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+	// $('.slider').slick();
+
+	$('.slider').slick({
+	  arrows : false,
+	  dots: true,
+	});
+
+
+	$('.carousel').slick({
+	  arrows : true,
+	  dots: true,
+	  // autoplay: true,
+  	//   autoplaySpeed: 4000,
+	});
+
+
+
+});
